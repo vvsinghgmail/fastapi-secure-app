ba280c88c946d95bbefcd8769c4ddf19a2801f82
vvsinghgmail <vishalvsingh@gmail.com>
2025-11-23 11:18:57 +0000

    chaged app.py

--- app/main.py
+++ app/main.py
@@ -1,14 +1,14 @@
 from fastapi import FastAPI
 
-from app.db.session import init_db
 from app.api.v1 import auth, blogs
+# from app.db.session import init_db  # not needed for UAT with Alembic
 
 
 def create_app() -> FastAPI:
     app = FastAPI(title="Secure FastAPI App", version="1.0.0")
 
-    # Dev/UAT convenience – in real prod rely on Alembic only
-    init_db()
+    # In UAT/Prod we rely on Alembic migrations, not init_db()
+    # init_db()
 
     app.include_router(auth.router, prefix="/api/v1")
     app.include_router(blogs.router, prefix="/api/v1")
@@ -21,3 +21,4 @@ def create_app() -> FastAPI:
 
 
 app = create_app()
+
