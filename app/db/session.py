8453090cd25d84e4c88f83213af64203c596b037
vvsinghgmail <46373377+vvsinghgmail@users.noreply.github.com>
2025-11-23 15:34:20 +0530

    Update session.py

--- app/db/session.py
+++ app/db/session.py
@@ -1,4 +1,5 @@
 from sqlmodel import SQLModel, create_engine, Session
+
 from app.core.config import get_settings
 
 settings = get_settings()
@@ -6,7 +7,7 @@ engine = create_engine(settings.DATABASE_URL, echo=False)
 
 
 def init_db() -> None:
-    # Import models so they’re registered
+    # Import models so they register with SQLModel.metadata
     from app.models import user, blog, token  # noqa: F401
 
     SQLModel.metadata.create_all(bind=engine)
@@ -15,5 +16,3 @@ def init_db() -> None:
 def get_session():
     with Session(engine) as session:
         yield session
-
-
